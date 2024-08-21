fd3152de804d202d23dc1f382c8b820e6be34997
jeanFrancoGuerreroAcero <guerreroacerojeanfranco@gmail.com>
2024-08-21 12:54:09 -0500

    fixed :sparkles: super_digit

---
+++ Dia 2/script.js
@@ -0,0 +1,17 @@
+var num =prompt(":");
+var repeticiones= prompt(":")
+Arra=[];
+for (const i of num){
+    console.log[i];
+    var numeroo=Math.floor(i);
+    var subir=Arra.push(numeroo);
+}
+console.log(Arra);
+while (num>1){
+    for(var i in Arra){  
+        var ecuacion= Arra.reduce((a,b) => a +b);
+        console.log(ecuacion)
+        }
+    break
+}
+  
