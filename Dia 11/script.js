dcc53c19efbba1b1fb04ea3ecfc524578ef9ba61
jeanFrancoGuerreroAcero <guerreroacerojeanfranco>
2024-09-06 13:00:05 -0500

    fixed :sparkles: deveploment in work in class

--- Dia 11/script.js
+++ Dia 11/script.js
@@ -1,5 +1,19 @@
 const link = "https://randomuser.me/api/"
 mostar(link)
+mostarFoto(link)
+
+let cards = document.getElementsByClassName("imagen");
+let previous = cards[0];
+
+for(const card of cards){
+    card.addEventListener("mouseenter",() => {
+        if(!card.classList.contains("seleccion")){
+            previous.classList.remove("seleccion")
+            previous  = card;
+            card.classList.add("seleccion")
+        }
+    })
+}
 
 function mostar(link){
     fetch(link)
@@ -7,8 +21,81 @@ function mostar(link){
     .then(Data =>{
         let datos= document.getElementById("abaNombre")
         datos.innerHTML = `
-        <p id="ho">${Data.results[0]["name"]["title"]}</p>`
+        <p id="ho">${Data.results[0]["name"]["first"]}</p>`
     })
+    let datos= document.getElementById("nombre")
+    datos.innerHTML = `
+    <p>Hi, My name is</p>`
+}
+
+function mostarEmail(link){
+    fetch(link)
+    .then(res => res.json())
+    .then(Data =>{
+        let datos= document.getElementById("abaNombre")
+        datos.innerHTML = `
+        <p id="ho">${Data.results[0]["email"]}</p>`
+    })
+    let datos= document.getElementById("nombre")
+    datos.innerHTML = `
+    <p>Hi, My email is</p>`
+}
+
+function mostarMapa(link){
+    fetch(link)
+    .then(res => res.json())
+    .then(Data =>{
+        let datos= document.getElementById("abaNombre")
+        datos.innerHTML = `
+        <p id="ho">${Data.results[0]["location"]["street"]["number"]}-${Data.results[0]["location"]["street"]["name"]}</p>`
+    })
+    let datos= document.getElementById("nombre")
+    datos.innerHTML = `
+    <p>Hi, My address is</p>`
+}
+
+function mostarNumero(link){
+    fetch(link)
+    .then(res => res.json())
+    .then(Data =>{
+        let datos= document.getElementById("abaNombre")
+        datos.innerHTML = `
+        <p id="ho">${Data.results[0]["phone"]}</p>`
+    })
+    let datos= document.getElementById("nombre")
+    datos.innerHTML = `
+    <p>Hi, My number is</p>`
+}
+
+function mostarNacimiento(link){
+    fetch(link)
+    .then(res => res.json())
+    .then(Data =>{
+        let datos= document.getElementById("abaNombre")
+        datos.innerHTML = `
+        <p id="ho">${Data.results[0]["dob"]["date"]}</p>`
+    })
+    let datos= document.getElementById("nombre")
+    datos.innerHTML = `
+    <p>Hi, My birthday is</p>`
+}
+
+function mostarContraseña(link){
+    fetch(link)
+    .then(res => res.json())
+    .then(Data =>{
+        let datos= document.getElementById("abaNombre")
+        datos.innerHTML = `
+        <p id="ho">${Data.results[0]["login"]["password"]}</p>`
+    })
+    let datos= document.getElementById("nombre")
+    datos.innerHTML = `
+    <p>Hi, My password is</p>`
+}
+
+function mostarFoto(link){
+    fetch(link)
+    .then(res => res.json())
     .then(ata =>{
         let app= document.getElementById("foto")
         app.innerHTML = `
---
+++ Dia 12/script.js
@@ -0,0 +1,57 @@
+document.addEventListener(`DOMContentLoaded`,() =>{
+    const datosContenedor= document.querySelector(`.opciones`);
+    const taskInput = document.getElementById(`taskInput`);
+    const addTaskButton = document.getElementById(`addTaskButton`);
+
+    async function fectData(){
+        const res = await fetch(`https://6674179975872d0e0a950e53.mockapi.io/todoList`)
+        data = await res.json();
+        return data;
+    }
+
+    function displayCapsula(capsula){
+        datosContenedor.innerHTML=``;
+        capsula.forEach(cap => {
+            const capDiv = document.createElement(`div`)
+            if(cap.status === "ready"){
+                capDiv.classList.add(`capsulanegativa`);
+                capDiv.innerHTML = `
+                    <div class="capsulanegativa">
+                <div class="infotextnegativo">
+                        <p>${cap.task}</p>
+                    </div>
+                    <div class="botonesnegativo">
+                        <div class="terminadonegativo">
+                            <img src="./storage/img/pngwing.com (2).png" alt="" data-id="1" class="completado">
+                        </div>
+                        <div class="eliminadonegativo">
+                            <img src="./storage/img/pngwing.com (4).png" alt="" data-id="2">
+                        </div>
+                    </div>
+                </div>
+            `
+            }
+            if(cap.status === "on hold"){
+                capDiv.classList.add("capsula");
+                capDiv.innerHTML = `
+                <div class="capsula">
+            <div class="infotext">
+                <p>${cap.task}</p>
+            </div>
+            <div class="botones">
+                <div class="terminado">
+                    <img src="./storage/img/pngwing.com (2).png" alt="" data-id="1" class="completado">
+                </div>
+                <div class="eliminado">
+                    <img src="./storage/img/pngwing.com (4).png" alt="" data-id="2">
+                </div>
+            </div>
+        </div>`
+            }
+            datosContenedor.appendChild(capDiv)
+        });
+    }
+    fectData().then(data => {
+        displayCapsula(data);
+    })
+})
